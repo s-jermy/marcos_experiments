2bc9ac6620a21eb9c6d3b745453c7db3e417c75a
s-jermy <stephen.jermy@uct.ac.za>
2024-08-26 16:06:16 +0200

    test

---
+++ my_first_experiment.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib as plt
+import sdb
+
+st = pdb.set_trace
+
+import external
+import experiment as ex
+
+def my_first_experiment():
+    exp = ex.Experiment(lo_freq=5,rx_t=3.125)
+
+if __name__ = "__main__":
+    my_first_experiment()
