2a44c2d7f5aa98199f1e7e2803c1aa0ac9edc1a6
s-jermy <stephen.jermy@uct.ac.za>
2024-10-18 14:06:10 +0200

    completed tutorial 1

--- my_first_experiment.py
+++ my_first_experiment.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib as plt
-import sdb
+import matplotlib.pyplot as plt
+import pdb
 
 st = pdb.set_trace
 
@@ -10,5 +10,29 @@ import experiment as ex
 def my_first_experiment():
     exp = ex.Experiment(lo_freq=5,rx_t=3.125)
 
-if __name__ = "__main__":
+    event_dict = {
+            "tx0": (np.array([50,130,200,360]),np.array([0.5,0,0.5j,0])),
+            "tx1": (np.array([500,700]), np.array([0.2,0])),
+            "rx0_en": (np.array([400,800]), np.array([1,0])),
+            "rx1_en": (np.array([400,800]), np.array([1,0]))
+            }
+
+#    tx0_times = np.array([50, 130]) #start pulse at 50us and end at 130us
+#    tx0_amps = np.array([0.5, 0]) #set amplitude to half power
+    
+#    event_dict = {'tx0': (tx0_times, tx0_amps)} #create dictionary of events
+    exp.add_flodict(event_dict) #add to dictionary
+#    exp.add_flodict({'rx0_en': (np.array([200,400]),np.array([1,0]))}) #create readout entry
+    
+    exp.plot_sequence() #plot the sequence...
+    #plt.show() #and show it
+
+    rxd, msgs = exp.run() #run the experiment using the marcos server
+    exp.close_server(only_if_sim=True) #close the simulated server only
+
+    plt.figure()
+    plt.plot(np.abs(rxd["rx1"]))
+    plt.show()
+
+if __name__ == "__main__":
     my_first_experiment()
